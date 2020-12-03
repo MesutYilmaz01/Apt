be62e70483bc2eb98b0b8daae78a6f783d11d77c
MesutYilmaz <mesutyilmaz1993@gmail.com>
2020-12-03 12:17:37 +0300

    Manifest Uptaded

--- manifest.js
+++ manifest.js
@@ -1,32 +1,15 @@
-const CACHE ='AP'
-function installCB(e) {
-  console.log(CACHE, e);
+{
+  "name": "APT",
+  "short_name": "APT",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "orange",
+  "theme-color": "orange",
+  "icons": [
+    {
+      "src": "image/image.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
 }
-addEventListener('install', installCB)
-function save(req, resp) {
-  if (!req.url.includes("github")) 
-     return resp;
-  return caches.open(CACHE)
-  .then(cache => { // save request
-    cache.put(req, resp.clone());
-    return resp;
-  }) 
-  .catch(console.err)
-}
-function report(req) {
-  console.log(CACHE+' matches '+req.url)
-  return req
-}
-function fetchCB(e) { //fetch first
-  let req = e.request
-  e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => caches.match(req).then(report))
-  )
-}
-addEventListener('fetch', fetchCB)
-
-function activateCB(e) {
-  console.log(CACHE, e);
-}
-addEventListener('activate', activateCB);
